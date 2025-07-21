0659e830bc3a7e1c4550bdf5a08cea90894a8ac4
ahmed hamdi <ah2093@fayoum.edu.eg>
2025-07-21 21:35:52 +0300

    project setup

---
+++ src/main/java/eg/com/tokencomparison/TokenComparisonApplication.java
@@ -0,0 +1,13 @@
+package eg.com.tokencomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenComparisonApplication.class, args);
+    }
+
+}
